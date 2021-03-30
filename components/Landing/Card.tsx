5a6fb5958d19463491c3e8b0c94f154e9b8042e6
Ryan <rypoon18@gmail.com>
2021-03-29 20:33:52 -0400

    adjust card styling

--- components/Landing/Card.tsx
+++ components/Landing/Card.tsx
@@ -5,8 +5,8 @@ interface Props {
 
 const Card: React.FC<Props> = ({ children, reverse }) => {
     return (
-        <div className="md:w-3/5 mt-10 p-6 rounded-lg bg-white">
-            <div className={`flex flex-col-reverse md:flex-row items-center center md:text-left text-xl font-light text-gray-500`}>
+        <div className="md:w-2/3 mt-10 p-6 rounded-lg bg-white">
+            <div className={`flex flex-col-reverse md:flex-row items-center justify-around md:text-left text-xl font-light text-gray-500`}>
                 { children }
             </div>
         </div>
--- components/Landing/Navbar.tsx
+++ components/Landing/Navbar.tsx
@@ -6,14 +6,14 @@ const Navbar: React.FC = () => {
         <div className="flex justify-between sticky top-0 px-2 md:px-20 py-3 w-full bg-white z-50 ">
             <div className="flex items-center">
                 <img src={require("../../public/static/imgs/favicon-32x32.png")} alt="logo" className="inline"></img>
-                <h1 className="inline text-3xl font-bold font-sans">erenime</h1>
+                <h1 className="inline text-2xl font-bold font-sans">erenime</h1>
             </div>
             <div className="flex items-center">
                 <Link href="/login">
-                    <a className="transition duration-200 mr-2 md:mr-10 px-3 py-1.5 md:px-6 md:py-3 rounded-md bg-highlight text-white hover:bg-highlight-secondary" href="#">Login</a>
+                    <a className="transition duration-200 mr-2 md:mr-10 px-3 py-1.5 md:px-4 md:py-2 rounded-md bg-highlight text-white hover:bg-highlight-secondary" href="#">Login</a>
                 </Link>
                 <Link href="/signup">
-                    <a className="transition duration-200 px-3 py-1.5 md:px-6 md:py-3 rounded-md bg-highlight text-white hover:bg-highlight-secondary" href="#">Sign Up</a>
+                    <a className="transition duration-200 px-3 py-1.5 md:px-4 md:py-2 rounded-md bg-highlight text-white hover:bg-highlight-secondary" href="#">Sign Up</a>
                 </Link>
             </div>
         </div>
