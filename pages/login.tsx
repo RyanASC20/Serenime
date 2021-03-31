df58e4e44d3576810e51ade88a6085054f1db81b
Ryan <rypoon18@gmail.com>
2021-03-31 16:28:17 -0400

    change heading font

--- pages/login.tsx
+++ pages/login.tsx
@@ -13,7 +13,7 @@ const SignUpPage: React.FC = () => {
         <Navbar />
         <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
           <div className="text-center mt-24">
-            <h2 className="mt-6 text-center text-3xl leading-9 font-extrabold text-highlight">
+            <h2 className="mt-6 text-center text-3xl leading-9 font-sans2 font-extrabold text-highlight">
               Welcome Back
           </h2>
             <p className="mt-2 text-center text-md text-gray-500">
