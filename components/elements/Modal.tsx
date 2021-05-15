002f8f165a5c3bcf1fabf696a73df3a8f25c0d95
Ryan <rypoon18@gmail.com>
2021-05-14 20:29:03 -0400

    lower modal opacity

--- components/elements/Modal.tsx
+++ components/elements/Modal.tsx
@@ -7,7 +7,7 @@ interface ModalProps {
 const Modal: React.FC<ModalProps> = ({ children, close }) => {
     return (
         <>
-            <div className="fixed flex justify-center items-center z-50 top-0 left-0 w-screen h-screen bg-black opacity-90">
+            <div className="fixed flex justify-center items-center z-50 top-0 left-0 w-screen h-screen bg-black opacity-75">
                 
             </div>
             <div className="absolute z-50 w-full md:w-2/3 flex flex-col rounded-lg p-2 md:p-5 bg-base">
