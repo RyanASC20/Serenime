a076a85e0148ae3b60823bc989dd7db202b72bc7
Ryan <rypoon18@gmail.com>
2021-05-14 20:25:58 -0400

    change tooltip to modal

--- components/elements/Tooltip.tsx
+++ components/elements/Tooltip.tsx
@@ -1,14 +1,22 @@
 import { useState } from "react";
 
+import Modal from "./Modal";
+
 const Tooltip: React.FC = ({ children }) => {
-    const [ active, setActive ] = useState(false);
+    const [active, setActive] = useState(false);
 
     return (
-        <>  
-            <p className="inline-flex justify-center items-center ml-3 w-5 h-5 rounded-full border border-gray-500 text-gray-500 text-sm cursor-pointer" onClick={() => { setActive(!active)} } onMouseOver={() => { setActive(true) }} onMouseOut={() => { setActive(false) } }>i</p>
-            <div className={`transition duration-200 p-3 absolute ${active ? 'opacity-100' : 'opacity-0 pointer-events-none'} z-50 border border-gray-500 rounded-md bg-highlight-secondary text-sm text-white`}>
-                { children }
-            </div>
+        <>
+            <p className="inline-flex justify-center items-center ml-3 w-5 h-5 rounded-full border border-gray-500 text-gray-500 text-sm cursor-pointer" onClick={() => { setActive(!active) }}>
+                i
+                {/* <div className={`transition duration-200 p-3 absolute ${active ? 'opacity-100' : 'opacity-0 pointer-events-none'} z-50 border border-gray-500 rounded-md bg-highlight-secondary text-sm text-white`}>
+                    {children}
+                </div> */}
+            </p>
+            { active && <Modal close={() => { setActive(false) }}>
+                {children}
+            </Modal>}
+
         </>
     )
 }
--- pages/picture.tsx
+++ pages/picture.tsx
@@ -1,12 +1,18 @@
 import PicturePage from '../components/Layouts/PicturePage';
 import PowerPicture from "../components/modules/PowerPicture";
 import PageHeading from '../components/elements/PageHeading';
+import Tooltip from '../components/elements/Tooltip';
 
 const Picture: React.FC = () => {
     return (
         <PicturePage title="Picture">
             <div>
                 <PageHeading title="Picture" />
+                <Tooltip>
+                    <p>Choose a picture that represents your goals!</p>
+                    <p>Then, try to make as many observations about it as possible? What about it speaks to you?</p>
+                    <p>Get the image URL by going searching for images, right clicking, and selecting <span className="font-bold">"Copy image link"</span></p>
+                </Tooltip>
                 <PowerPicture />
             </div>
         </PicturePage>
