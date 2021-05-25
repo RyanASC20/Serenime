3cb6fdc90e22e03c7bc35e387cdb1183ac1c5b76
Ryan <rypoon18@gmail.com>
2021-05-25 15:16:59 -0400

    change card ui

--- components/elements/LandingCard/Card.tsx
+++ components/elements/LandingCard/Card.tsx
@@ -1,13 +1,19 @@
 
-interface Props {
+interface CardProps {
     reverse?: any;
+    title: string;
+    description: string;
 }
 
-const Card: React.FC<Props> = ({ children, reverse }) => {
+const Card: React.FC<CardProps> = ({ children, description, title, reverse }) => {
     return (
-        <div className="w-11/12 md:w-2/3 mt-10 p-3 md:p-6 rounded-t-md bg-white">
-            <div className={`flex flex-col md:flex-row items-center justify-around md:text-left text-xl font-light font-sans2 text-gray-500`}>
-                { children }
+        <div className="w-11/12 md:w-2/3 mt-10 p-3 md:p-6 my-6">
+            <div className={`flex flex-col ${reverse ? "md:flex-row-reverse" : ""} md:flex-row items-center justify-around md:text-left text-xl font-light font-sans2 text-gray-500`}>
+                <div className="max-w-md">
+                    <h1 className="text-highlight-secondary my-5 text-5xl font-bold text-gray-700">{title}</h1>
+                    <p>{description}</p>
+                </div>
+                {children}
             </div>
         </div>
     )
--- pages/index.tsx
+++ pages/index.tsx
@@ -22,8 +22,20 @@ const Index: React.FC = () => {
                         <img className="shadow-lg rounded-lg w-11/12 md:w-3/5" src={require("../public/static/imgs/landing-home.jpg")}></img>
                     </div>
                 </section>
-                <section className="flex flex-col items-center mb-10">
-                    <Card>
+                <section className="flex flex-col items-center my-28">
+                    <Card description="Simply enter how you feel and viisualize how you've been." title="Track Your Mood">
+                        <img src={require("../public/static/imgs/moodtracker.jpg")} className="max-w-xs md:max-w-xl"></img>
+                    </Card>
+                    <Card reverse={true} title="Breathe" description="Take some time out of your day to just relax with our guided breathing exercises.">
+                        <img src={require("../public/static/imgs/breathing.jpg")} className="max-w-xs md:max-w-xl"></img>
+                    </Card>
+                    <Card title="Track Your Goals" description="If you have trouble staying consistent, maybe visualizing your goals on a calendar will help!">
+                        <img src={require("../public/static/imgs/goals.jpg")} className="max-w-xs md:max-w-xl"></img>
+                    </Card>
+                    <Card title="Plan Your Day" reverse={true} description="It's important to build good habits. Organize your morning and evening routines with Serenime.">
+                        <img src={require("../public/static/imgs/routines.jpg")} className="max-w-xs md:max-w-xl"></img>
+                    </Card>
+                    {/* <Card>
                         <div className="md:w-1/2">
                             <h1 className="my-6 text-4xl text-center md:text-left font-sans text-highlight-secondary">Mood Tracker</h1>
                             <p className="text-center md:text-left">Track how you feel using Serenime's mood tracker. With a simple glance at the calendar, you can see how you've been this month wihtout having to spend time reading through notes!</p>
@@ -54,7 +66,7 @@ const Index: React.FC = () => {
                             <p className="text-center md:text-left">The planner will automatically color code and sort each event based on its deadline, so you don't have to worry about becoming overwhelmed or doing tasks at the last minute!</p>
                         </div>
                         <img src={require("../public/static/imgs/routines.jpg")} className="max-w-xs mt-4 md:max-w-sm"></img>
-                    </Card>
+                    </Card> */}
                 </section>
                 <Footer />
             </div>
